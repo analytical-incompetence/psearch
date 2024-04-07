300a2ede3bea5c44be5c83faf54e1ef6db728b1e
Jelahu <ljhurd2004@outlook.com>
2024-04-07 15:38:25 +0100

    Added Search Box to Search Page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,8 @@
 import { getServerAuthSession } from "@/server/auth";
 import { unstable_noStore as noStore } from "next/cache";
-import Link from "next/link";
 import { redirect } from 'next/navigation';
 import { Header } from "../_components/header";
+import { SearchBox } from "../_components/new-search";
 
 
 export default async function Search() {
@@ -20,20 +20,7 @@ export default async function Search() {
             <h1 className="z-20 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
               <span className="text-[hsl(280,100%,70%)]">Psearch Search Page</span>
             </h1>
-            <div className="z-20 flex flex-col items-center gap-2">
-
-              <div className="z-20 flex flex-col items-center justify-center gap-4">
-                <p className="text-center text-2xl text-white">
-                  {session && <span>Logged in as {session.user?.name}</span>}
-                </p>
-                <Link
-                  href={session ? "/api/auth/signout?callbackUrl=/" : "/api/auth/signin?callbackUrl=/search"}
-                  className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-                >
-                  {session ? "Sign out" : "Sign in"}
-                </Link>
-              </div>
-            </div>
+            <SearchBox/>
           </div>
         </main>
       </div>
