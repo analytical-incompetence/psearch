4f2af7a0b5b82089c31ed79a87766c1fadf7308e
Jelahu <ljhurd2004@outlook.com>
2024-04-12 18:45:59 +0100

    history page top of screen

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,8 +1,8 @@
-import {getServerAuthSession} from "@/server/auth";
-import {unstable_noStore as noStore} from "next/cache";
-import {redirect} from 'next/navigation';
-import {Header} from "../_components/header";
-import {SearchHistory} from "../_components/search-history";
+import { getServerAuthSession } from "@/server/auth";
+import { unstable_noStore as noStore } from "next/cache";
+import { redirect } from 'next/navigation';
+import { Header } from "../_components/header";
+import { SearchHistory } from "../_components/search-history";
 
 
 export default async function History() {
@@ -15,7 +15,7 @@ export default async function History() {
             <div>
                 <Header/>
                 <main
-                    className="overflow-hidden flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+                    className="overflow-hidden flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
                     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                         <h1 className="z-20 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
                             <span className="text-[hsl(280,100%,70%)]">pSearch</span>
