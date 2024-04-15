00f3e933e83a5ca6d7fa16f673e230c8e0b95b7c
Toby Davis <pencilcaseman@gmail.com>
2024-04-15 16:14:31 +0100

    Update History page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -18,7 +18,7 @@ export default async function History() {
                     className="overflow-hidden flex min-h-screen flex-col items-center text-white">
                     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                         <h1 className="z-20 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-                            <span className="text-[hsl(280,100%,70%)]">History</span>
+                            History
                         </h1>
                     </div>
                     <div style={{
