2276199681f307e9a87548b19171d6fe2f67dece
Jelahu <ljhurd2004@outlook.com>
2024-04-21 20:51:38 +0100

    Finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,56 @@
 "use client";
-import { Header } from "./_components/header";
-import { BackgroundGradientAnimation } from "./_components/ui/background-gradient-animation";
+import { Button } from "@nextui-org/react";
+import { motion } from "framer-motion";
+import Link from "node_modules/next/link";
+import { LampContainer } from "./_components/ui/lamp";
 
 export default function Home() {
 
     return (
         <div>
-            <Header session={null} allow_login={false}/>
-            <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-                pSearch
-            </p>
-            </div>
-        </BackgroundGradientAnimation>
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        pSearch
+      </motion.h1>
+      <motion.p
+              initial={{ opacity: 0.5, y: 100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+              }}
+              className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-sm font-medium tracking-tight text-transparent md:text-5xl">
+                The only web browser you will ever need.
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center font-medium tracking-tight text-transparent">
+        <Link href="/search">
+            <Button className="p-[3px] relative">
+                <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                Click Me!
+                </div>
+            </Button>
+        </Link>
+      </motion.div>
+    </LampContainer>
       </div>
     );
 }
