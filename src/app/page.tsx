fcb995c6846525eb3a5306ebe0903bae538158ea
Jelahu <ljhurd2004@outlook.com>
2024-04-21 19:38:03 +0100

    Made home page client side render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import { Header } from "@/app/_components/header";
-import { getServerAuthSession } from "@/server/auth";
-import { api } from "@/trpc/server";
-import { unstable_noStore as noStore } from "next/cache";
+"use client";
 
-export default async function Home() {
-    noStore();
-    const hello = await api.post.hello.query({text: "from AI"});
-    const session = await getServerAuthSession();
+
+export default function Home() {
 
     return (
         <div>
-            <Header session={session}/>
             <main
                 className={"overflow-hidden flex min-h-screen flex-col items-center justify-center text-white"}
             >
@@ -18,17 +12,6 @@ export default async function Home() {
                     <h1 className="z-20 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
                         <span className="text-[hsl(280,100%,70%)]">pSearch</span>
                     </h1>
-                    <div className="z-20 flex flex-col items-center gap-2">
-                        <p className="text-2xl text-white">
-                            {hello ? hello.greeting : "Loading tRPC query..."}
-                        </p>
-
-                        <div className="z-20 flex flex-col items-center justify-center gap-4">
-                            <p className="text-center text-2xl text-white">
-                                {session && <span>Logged in as {session.user?.name}</span>}
-                            </p>
-                        </div>
-                    </div>
                 </div>
             </main>
         </div>
